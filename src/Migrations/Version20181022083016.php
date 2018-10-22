c2e9fea80defccd41946f05cd56611f6b0887d78
marianoSP <mariano.sanchez@ebolution.com>
2018-10-22 10:41:42 +0200

    Tema 5 Ejercicio 1

---
+++ src/Migrations/Version20181022083016.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181022083016 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE noticia ADD texto_noticia LONGTEXT NOT NULL, ADD texto_titular LONGTEXT NOT NULL, ADD imagen VARCHAR(200) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE noticia DROP texto_noticia, DROP texto_titular, DROP imagen');
+    }
+}
