b8265d8da8041ada68ab5efb63158ee575fdf5e9
marianoSP <mariano.sanchez@ebolution.com>
2018-10-26 13:50:28 +0200

    Tema 8 Ejercicio 1

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -8,12 +8,16 @@
 
 namespace App\Controller;
 
+use App\Repository\NoticiaRepository;
 use App\Entity\Noticia;
+use App\Entity\Usuario;
+use App\Form\Login;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class DeportesController extends Controller{
 
@@ -25,28 +29,61 @@ class DeportesController extends Controller{
         return $this->render('base.html.twig');
     }
 
-    /*
+
         /**
          * @Route("/deportes/usuario", name="usuario" )
          */
-/*
+
     public function sesionUsuario(Request $request) {
         $usuario_get=$request->query->get('nombre');
         $session = $request->getSession();
         $session->set('nombre', $usuario_get);
         return $this->redirectToRoute('usuario_session',array('nombre'=>$usuario_get));
     }
+
     /**
      * @Route("/deportes/usuario/{nombre}", name="usuario_session" )
      */
-/*
+
     public function paginaUsuario() {
         $session=new Session();
         $usuario=$session->get('nombre');
         return new Response(sprintf('Sesión iniciada con el atributo nombre: %s', $usuario
         ));
     }
-*
+    /**
+     * @Route("/deportes/nuevousuario", name="usuariobd")
+     */
+    public function nuevoUsuarioBd()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $usuario=new Usuario();
+        $usuario->setEmail("jose@imaginaformacion.com");
+        $usuario->setUsername("jose");
+        $password = $this->get('security.password_encoder')
+            ->encodePassword($usuario, "imaginapass");
+        $usuario->setPassword($password);
+        $em->persist($usuario);
+        $em->flush();
+        return new Response("Usuario guardado!");
+    }
+
+    /**
+     * @Route("/deportes/login", name="login_seguro" )
+     */
+    public function loginUsuario(Request $request, AuthenticationUtils $authUtils)
+    {
+        // Capturamos el error de autenticación
+        $error = $authUtils->getLastAuthenticationError();
+        // Último nombre de usuario autenticado
+        $lastUsername = $authUtils->getLastUsername();
+        return $this->render('Security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /*
     /**
      * @Route("/deportes/{section}/{page}", name="lista_paginas",
      *      requirements={"page"="\d+"},
@@ -88,10 +125,10 @@ class DeportesController extends Controller{
         return new Response(sprintf('Mi noticia en idioma=%s, fecha=%s,deporte=%s,equipo=%s, noticia=%s ', $_locale, $date, $section, $team, $slug));
     }
 */
-    /**
+  /*  /**
     * @Route("/deportes/cargabd", name="noticia")
     */
-
+/*
     public function cargarBD(){
         $em = $this->getDoctrine()->getManager();
 
@@ -112,6 +149,7 @@ class DeportesController extends Controller{
     /**
     * @Route("/deportes/actualizar", name="actualizarNoticia")
     */
+/*
     public function actualizarBd(Request $request) {
         $em=$this->getDoctrine()->getManager();
         $id=$request->query->get('id');
@@ -127,7 +165,7 @@ class DeportesController extends Controller{
 
     /**
      * @Route("/deportes/eliminar", name="actualizarNoticia")
-     */
+     *//*
     public function eliminarBd(Request $request) {
         $em=$this->getDoctrine()->getManager();
         $id=$request->query->get('id');
@@ -142,7 +180,7 @@ class DeportesController extends Controller{
      * @Route("/deportes/{seccion}/{pagina}", name="lista_paginas",
      *      requirements={"pagina"="\d+"},
      *      defaults={"seccion":"tenis"})
-    */
+    *//*
     public function lista($pagina = 1, $seccion) {
         $em=$this->getDoctrine()->getManager();
         $repository = $this->getDoctrine()->getRepository(Noticia::class);
@@ -166,7 +204,7 @@ class DeportesController extends Controller{
     /**
      * @Route("/deportes/{seccion}/{titular} ",
      * defaults={"seccion":"tenis"}, name="verNoticia")
-     */
+     *//*
     public function noticia($titular, $seccion)
     {
         $em=$this->getDoctrine()->getManager();
@@ -191,6 +229,7 @@ class DeportesController extends Controller{
         ]);
     }
 
+*/
 
 
 }
